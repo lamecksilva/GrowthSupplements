cb243f178280fa9732200724f1018573b9b50f44
Lameck Sandro <lameck2@hotmail.com>
2021-09-02 16:09:08 -0300

    FEAT: Add SafeAreaProvider to Container

--- src/presentation/Routes.tsx
+++ src/presentation/Routes.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomePage } from './pages/Home/Home';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 export function Routes(): JSX.Element {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomePage} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomePage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </>
   );
 }
