ccb06a73855f1ae3ee240db92dbedc088ac82763
Lameck Sandro <lameck2@hotmail.com>
2021-09-03 16:24:23 -0300

    FIX: Move redux hooks

--- src/domain/store.ts
+++ src/domain/store.ts
@@ -1,4 +1,5 @@
-import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import globalReducer from './reducers/global';
 
@@ -8,6 +9,10 @@ export const store = configureStore({
     // counter: counterReducer,
   },
 });
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
