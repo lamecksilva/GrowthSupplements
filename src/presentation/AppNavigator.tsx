6ae816f3496ca0be88799940b5996b92037724f3
Lameck Sandro <lameck2@hotmail.com>
2021-09-03 00:47:37 -0300

    FIX: Broken dependencie react-native-reanimated

--- src/presentation/AppNavigator.tsx
+++ src/presentation/AppNavigator.tsx
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { HomePage } from './pages/Home';
+import { OrderPage } from './pages/Orders';
+import { ProfilePage } from './pages/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +12,8 @@ function StackNavigator(): JSX.Element {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomePage} />
+      <Stack.Screen name="Order" component={OrderPage} />
+      <Stack.Screen name="Profile" component={ProfilePage} />
     </Stack.Navigator>
   );
 }
--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export function HomePage(): JSX.Element {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Text>Home Page</Text>
+
+      <Button onPress={() => navigation.navigate('Order')}>Order</Button>
     </SafeAreaView>
   );
 }
