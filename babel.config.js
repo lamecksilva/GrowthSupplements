95e29e7f7232a86bbc2100edc822d4eccefb8a75
Lameck Sandro <lameck2@hotmail.com>
2021-09-05 14:44:10 -0300

    FEAT: Add module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@domain': './src/domain',
+          '@infra': './src/infra',
+          '@presentation': './src/presentation',
+        },
+      },
+    ],
+  ],
 };
